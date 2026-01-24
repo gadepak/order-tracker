cd607493fae55ddde32a523bd3f64ec4a84c4347
Gajawada Aravindh <gajawadaaravindh4114@gmail.com>
2026-01-24 22:35:29 +0530

    Fix MySQL connection (Railway internal)

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,14 +1,14 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port:  3306,
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
-module.exports = pool;
+module.exports = pool.promise();
